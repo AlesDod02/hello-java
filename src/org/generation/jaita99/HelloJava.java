7bcc1a8cf31aa1913720673ec541e98c66cfc2b8
AlesDod02 <alessandro.dod@gmail.com>
2023-11-28 12:53:48 +0100

    starting commit

---
+++ src/org/generation/jaita99/HelloJava.java
@@ -0,0 +1,12 @@
+package org.generation.jaita99;
+
+public class HelloJava {
+    private static String helloJaita99;
+
+    public static void main(String[] args) {
+        //qui è dove scrivo il mio codice eseguibile
+        System.out.println("helloJaita99");
+        System.out.println("Ciao come va");
+    }
+
+}
